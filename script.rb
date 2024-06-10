405d7f7c4dfbc3c4e9d4a5dd246788988115f049
Midlife Mechanicus <42699076+MidlifeMechanicus@users.noreply.github.com>
2024-06-10 07:43:35 +0000

    Create basic process for single word input

--- script.rb
+++ script.rb
@@ -1,6 +1,14 @@
-word = "below"
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"] 
+# Turn word into an array
+# Compare dictionary to array and push to new array for each inclusion
+# Then tally
 
-def substrings (word, dictionary) 
-    dictionary.select { |substring| word.include? substring }
-end
+word = "below" 
+p word
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"] 
+p dictionary
+
+included_words = dictionary.select { |substring| word.include? substring }
+p included_words
+
+word_count = included_words.tally
+p word_count
